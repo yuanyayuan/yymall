7e9fb5a9783bf3957a77753741478c623ef9bd3b
LiYuan <1747560489@qq.com>
2020-09-10 23:59:05 +0800

    mybatis配置类

---
+++ mall_backend_api/src/main/java/com/nexus/mall/api/config/MyBatisConfig.java
@@ -0,0 +1,11 @@
+package com.nexus.mall.api.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.nexus.mall.dao"})
+public class MyBatisConfig {
+}
